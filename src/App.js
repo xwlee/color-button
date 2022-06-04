cb04e196aa6a7b3970448757d4ba76414a53ff45
Lee Xiang Wei <688247+xwlee@users.noreply.github.com>
2022-06-04 18:35:20 +0800

    Add label to button

--- src/App.js
+++ src/App.js
@@ -20,11 +20,12 @@ function App() {
       </button>
       <input
         type="checkbox"
-        id="enable-button-checkbox"
+        id="disable-button-checkbox"
         defaultChecked={disabled}
         aria-checked={disabled}
         onChange={(e) => setDisabled(e.target.checked)}
       />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -34,12 +34,12 @@ test("initial conditions", () => {
 test("checkbox disables button on first click and enables on second click", () => {
   render(<App />);
 
-  const checkbox = screen.getByRole("checkbox");
-  const button = screen.getByRole("button");
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   fireEvent.click(checkbox);
-  expect(button).toBeDisabled();
+  expect(colorButton).toBeDisabled();
 
   fireEvent.click(checkbox);
-  expect(button).toBeEnabled();
+  expect(colorButton).toBeEnabled();
 });
