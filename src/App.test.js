a2ce61121d6acdb6ccd8e967a65058f6ee553d56
Lee Xiang Wei <688247+xwlee@users.noreply.github.com>
2022-06-04 16:01:18 +0800

    Test and code button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe("Change to red");
 });
